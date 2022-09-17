6fd729dc64451da8af178b3e7a77d1e11de80c7f
Alex <alex@globeit.io>
2022-09-17 15:03:12 +0600

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgxMatCalendarModule } from '@arjentienkamp/ngx-mat-calendar'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './src/app/home-page/home-page.component';
+import { CalendarComponent } from './src/app/calendar/calendar.component';
+import { NavbarComponent } from './src/app/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    CalendarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxMatCalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
