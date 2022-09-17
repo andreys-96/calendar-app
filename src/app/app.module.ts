897868de238a013a32398e399a8f43ac256c2612
Alex <alex@globeit.io>
2022-09-17 16:23:24 +0600

    add event modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomePageComponent } from './src/app/home-page/home-page.component';
 import { CalendarComponent } from './src/app/calendar/calendar.component';
 import { NavbarComponent } from './src/app/navbar/navbar.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AddEventComponent } from './src/app/add-event/add-event.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
+    AddEventComponent,
     CalendarComponent,
   ],
   imports: [
--- src/app/src/app/calendar/calendar.component.ts
+++ src/app/src/app/calendar/calendar.component.ts
@@ -1,27 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarOptions } from '@arjentienkamp/ngx-mat-calendar'
+import { CalendarOptions } from '@arjentienkamp/ngx-mat-calendar';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap'
+import { AddEventComponent } from '../add-event/add-event.component';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: './calendar.component.html',
-  styleUrls: ['./calendar.component.scss']
+  styleUrls: ['./calendar.component.scss'],
 })
 export class CalendarComponent implements OnInit {
   calendarOptions: CalendarOptions;
   events: any[] = [];
   date: Date = new Date()
-  constructor() {
+  constructor(
+    private modal: NgbModal
+  ) {
     this.calendarOptions = new CalendarOptions()
    }
 
+  //   title: string;
+  //   date: Date;
+  //   startTime: Date;
+  //   endTime: Date;
+  //   allDay?: boolean;
+  //   color?: any;
+  //   grid?: CalendarEventGrid;
+  //   location?: string;
+
   ngOnInit(): void {
-    console.log(this.date)
+    this.events.push({title: 'test', date: Date(), allDay: true, color: 'red', startTime: Date(), endTime: Date()})
+    console.log(this.events)
   }
 
   handleEventClick(event: any){}
   handleDateChange(event: any){}
   handleAddButtonClick(){
-    
+    this.modal.open(AddEventComponent, {centered: true, backdrop: true})
   }
 
 }
